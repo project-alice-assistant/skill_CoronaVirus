9c0e8b43a30661779e3892f8db28f2a8fc074deb
Psychokiller1888 <laurentchervet@bluewin.ch>
2020-03-16 13:22:46 +0100

    first version

---
+++ CoronaVirus.py
@@ -0,0 +1,84 @@
+import requests
+
+from core.ProjectAliceExceptions import SkillStartingFailed
+from core.base.model.AliceSkill import AliceSkill
+from core.dialog.model.DialogSession import DialogSession
+from core.util.Decorators import IntentHandler
+
+
+class CoronaVirus(AliceSkill):
+	"""
+	Author: Psycho
+	Description: Information about the spread of the virus, worldwide
+	"""
+
+	def __init__(self):
+		super().__init__()
+
+
+	def onStart(self):
+		super().onStart()
+		if not self.getConfig('apiKey'):
+			raise SkillStartingFailed('Missing API key')
+
+
+	@IntentHandler('GetCoronaVirusSpreadInfo')
+	def getInfo(self, session: DialogSession, **_kwargs):
+		if 'Country' not in session.slots:
+			country = self.getConfig('country').title()
+		else:
+			country = session.slotValue('country')
+
+		req = requests.get(
+			url='https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats',
+			params={
+				'country': country
+			},
+			headers={
+				'X-RapidAPI-Key': self.getConfig('apiKey')
+			}
+		)
+
+		if req.status_code != 200:
+			self.endDialog(
+				sessionId=session.sessionId,
+				text=self.randomTalk(text='error')
+			)
+			return
+
+		answer = req.json()
+
+		if not answer or 'data' not in answer or 'covid9Stats' not in answer['data']:
+			self.endDialog(
+				sessionId=session.sessionId,
+				text=self.randomTalk(text='error')
+			)
+			return
+
+		if len(answer['data']['covid9Stats']) > 1:
+			found = dict()
+			for worldCountry in answer['data']['covid9Stats']:
+				if worldCountry['country'].lower() == country.lower():
+					found = worldCountry
+					break
+
+			if not found:
+				self.endDialog(
+					sessionId=session.sessionId,
+					text=self.randomTalk(text='noData', replace=[country])
+				)
+				return
+		else:
+			found = answer['data']['covid9Stats'][0]
+
+		self.endDialog(
+			sessionId=session.sessionId,
+			text=self.randomTalk(text='situation', replace=[
+				country,
+				found['confirmed'],
+				found['deaths'],
+				found['recovered']
+			])
+		)
+		return
+
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# skill_CoronaVirus
-Real time information about the spread of the Corona virus
+# CoronaVirus
+
+[![Continous Integration](https://gitlab.com/project-alice-assistant/skills/skill_CoronaVirus/badges/master/pipeline.svg)](https://gitlab.com/project-alice-assistant/skills/skill_GarageDoorControl/pipelines/latest)
+
+### Description
+Information about the spread of the virus, worldwide
+
+- Version: 0.0.1
+- Author: Psycho
+- Maintainers:
+- Alice minimu: Version: 1.0.0-a6
+- Conditions:
+  - en
+- Requirements:
+
